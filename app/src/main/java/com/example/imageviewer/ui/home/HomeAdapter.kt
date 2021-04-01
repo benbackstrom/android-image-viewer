11d98992e2f409c35d20d239689bc007b7804dd7
Ben Backstrom <benbackstrom@gmail.com>
2021-04-01 10:38:20 -0700

    Get image file loading to work

--- app/src/main/java/com/example/imageviewer/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/imageviewer/ui/home/HomeAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.imageviewer.ui.home
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -9,26 +11,29 @@ import com.example.imageviewer.R
 import com.example.imageviewer.databinding.HomeListItemBinding
 import com.example.imageviewer.model.Image
 import com.squareup.picasso.Picasso
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import java.io.File
 import javax.inject.Inject
 
-class HomeAdapter @Inject constructor(): RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+class HomeAdapter @Inject constructor(): RecyclerView.Adapter<HomeViewHolder>() {
 
-    @Inject
-    lateinit var directoryProvider: DirectoryProvider
     var images: List<Image> = emptyList()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = HomeListItemBinding.inflate(inflater, parent, false)
-        return ViewHolder(binding)
+        return HomeViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
         if (position < images.size) {
             val image = images[position]
             holder.applyData(image)
@@ -36,26 +41,34 @@ class HomeAdapter @Inject constructor(): RecyclerView.Adapter<HomeAdapter.ViewHo
     }
 
     override fun getItemCount() = images.size
+}
 
-    class ViewHolder(private val binding: HomeListItemBinding): RecyclerView.ViewHolder(binding.root) {
+class HomeViewHolder(private val binding: HomeListItemBinding): RecyclerView.ViewHolder(binding.root) {
 
-        @Inject
-        lateinit var directoryProvider: DirectoryProvider
-
-        fun applyData(image: Image) {
-            loadImage(image, binding.imageView)
-            binding.titleView.text = image.title
-            binding.fileNameView.text = image.fileName
-        }
-
-        private fun loadImage(image: Image, imageView: ImageView) {
-            val rootPath = directoryProvider.directory?.path ?: return
-            val localImageUri = rootPath + File.separator + image.fileName
-
-            Picasso.get()
-                .load(localImageUri)
-                .placeholder(R.drawable.ic_baseline_image_24)
-                .into(imageView)
-        }
+    private val directoryProvider: DirectoryProvider by lazy {
+        EntryPointAccessors.fromApplication(binding.root.context, DirectoryProviderEntryPoint::class.java).provideDirectoryProvider()
     }
+
+    fun applyData(image: Image) {
+        loadImage(image, binding.imageView)
+        binding.titleView.text = image.title
+        binding.fileNameView.text = image.fileName
+    }
+
+    private fun loadImage(image: Image, imageView: ImageView) {
+        val rootPath = directoryProvider.directory?.path ?: return
+        val localImageUri = "file://" + rootPath + File.separator + image.fileName
+        Log.i("localImageUri", localImageUri)
+
+        Picasso.get()
+            .load(localImageUri)
+            .placeholder(R.drawable.ic_baseline_image_24)
+            .into(imageView)
+    }
+}
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface DirectoryProviderEntryPoint {
+    fun provideDirectoryProvider(): DirectoryProvider
 }
